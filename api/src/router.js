d7c0fd1437c126c9620678b1a098026ea5cd2cbc
Nicole Graham <FieldsOfSeconds@hotmail.com>
2022-08-19 15:47:42 +0100

    test

--- api/src/router.js
+++ api/src/router.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req,file,callback => {
     if(file.mimetype !== 'image/png') {
-        request.fileValidationError = 'Wrong file type.';
+        req.fileValidationError = 'Wrong file type.';
         callback(null, false, new Error('Wrong file type.'));
     } else {
         callback(null, true);
