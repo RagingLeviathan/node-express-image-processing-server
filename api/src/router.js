971b61ab90212976ac0e33e3c231c5f72d6a4b8f
Nicole Graham <FieldsOfSeconds@hotmail.com>
2022-08-19 15:43:39 +0100

    test

--- api/src/router.js
+++ api/src/router.js
@@ -1,33 +1,39 @@
-const { response } = require('express');
-const express = require('express');
+const { Router } = require('express');
 const multer = require('multer');
+const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
-const router = express.Router();
+const router = Router();
 
-const filename = 'fish';
-// (file, callback => {
-//     callback(null, file.originalname);
-// });
+const filename = (req, file, callback) => {
+    callback(null, file.originalname);
+};
 
-multer.diskStorage({destination: 'api/uploads', filename: filename});
+const storage = multer.diskStorage({
+    destination: 'api/uploads', 
+    filename: filename,
+});
 
-// fileFilter = (req,file,callback => {
-//     if(file.mimetype !== 'image/png') {
-//         request.fileValidationError = 'Wrong file type.';
-//         callback(null, false, {error: 'Wrong file type.'})
-//     } else {
-//         callback(null, true);
-//     }
-// });
+const fileFilter = (req,file,callback => {
+    if(file.mimetype !== 'image/png') {
+        request.fileValidationError = 'Wrong file type.';
+        callback(null, false, new Error('Wrong file type.'));
+    } else {
+        callback(null, true);
+    }
+});
 
-// const upload = multer({fileFilter: fileFilter, storage: storage});
+const upload = multer({
+    fileFilter: fileFilter, 
+    storage: storage,
+});
 
-// router.post('/upload',upload.single('photo'), (req,res) => {
-//     if(req.fileValidationError) {
-//         response.status(400).json({error: request.fileValidationError});
-//     } else {
-//         response.status(201).json({success: true});
-//     }
-// });
+router.post('/upload',upload.single('photo'), (req,res) => {
+    if(req.fileValidationError) {
+        return res.status(400).json({error: req.fileValidationError});
+    } else {
+        res.status(201).json({success: true});
+    }
+});
 
 module.exports = router;
