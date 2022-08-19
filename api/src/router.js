de8f9c5656cb1ff637a29cf6774f9986f109deeb
Nicole Graham <FieldsOfSeconds@hotmail.com>
2022-08-19 15:35:24 +0100

    test

--- api/src/router.js
+++ api/src/router.js
@@ -4,29 +4,30 @@ const multer = require('multer');
 
 const router = express.Router();
 
-const filename = (request, file, callback => {
-    callback(null, file.originalname);
-});
+const filename = 'fish';
+// (file, callback => {
+//     callback(null, file.originalname);
+// });
 
-multer.diskStorage({destination: 'api/uploads', filename: filename})
+multer.diskStorage({destination: 'api/uploads', filename: filename});
 
-fileFilter = (req,file,callback => {
-    if(file.mimetype !== 'image/png') {
-        request.fileValidationError = 'Wrong file type.';
-        callback(null, false, {error: 'Wrong file type.'})
-    } else {
-        callback(null, true);
-    }
-});
+// fileFilter = (req,file,callback => {
+//     if(file.mimetype !== 'image/png') {
+//         request.fileValidationError = 'Wrong file type.';
+//         callback(null, false, {error: 'Wrong file type.'})
+//     } else {
+//         callback(null, true);
+//     }
+// });
 
-const upload = multer({fileFilter: fileFilter, storage: storage});
+// const upload = multer({fileFilter: fileFilter, storage: storage});
 
-router.post('/upload',upload.single('photo'), (req,res) => {
-    if(req.fileValidationError) {
-        response.status(400).json({error: request.fileValidationError});
-    } else {
-        response.status(201).json({success: true});
-    }
-});
+// router.post('/upload',upload.single('photo'), (req,res) => {
+//     if(req.fileValidationError) {
+//         response.status(400).json({error: request.fileValidationError});
+//     } else {
+//         response.status(201).json({success: true});
+//     }
+// });
 
 module.exports = router;
